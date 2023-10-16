2f1c8c72dbb95ddaa9d67277be6e7bab6aab8964
lkj626 <hwi2337@naver.com>
2023-10-16 18:39:06 +0900

    Add files via upload

---
+++ hello.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+def add(x, y):
+    """This is an add function"""
+
+    return x + y
+
+
+print(add(1, 1))
+
+
+# In[ ]:
+
+
+
+
---
+++ test_hello.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+from hello import add
+
+
+def test_add():
+    assert 2 == add(1, 1)
+
+
+# In[ ]:
+
+
+
+
